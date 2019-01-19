d80249ce05b2926b9d44d19afc6482b5608b25ac
imerla1 <giorgi1048@yahoo.com>
2019-01-19 04:58:47 -0800

    Settings for main project

---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings for main project
+
+WIDTH = 600
+HEIGHT = 550
+title = "Shoot'em up"
+
+# Some basic colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+blues = (0, 0, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+fps = 30
